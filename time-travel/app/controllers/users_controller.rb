b51cdfd9c9873c9ff20eea5e8aacc9c80a7b9004
byfyang <darkspherico@gmail.com>
2016-06-20 15:07:34 -0700

    creates form/routes/controllers for users to sign up, creates user show page

--- time-travel/app/controllers/users_controller.rb
+++ time-travel/app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
 
-  def index
+  def home
     render :index
   end
+
+  def new
+  	@user = User.new
+  	render :new
+  end
+
+  def create
+  	user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+  	@user=User.create(user_params)
+
+  	redirect_to(user_id_path(@user.id))
+  end
+
+  def show
+  	@user = User.find(params[:id])
+    render :show
+  end
+
+
 
 end
--- time-travel/config/routes.rb
+++ time-travel/config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'users#index', as: 'home'
+  get '/', to: 'users#home', as: 'home'
+
+
+  ####route for new####
+  get '/signup', to: "users#new", as: "new_user"
+
+  post '/users', to: "users#create"
+
+  get '/users/:id', to: "users#show", as: "user_id"
+
+
 
 end
 
