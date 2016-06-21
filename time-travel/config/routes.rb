1310d754e20ea16bebe4e3a0ee8d72fc9550608d
Michael Norelli <mnorelli360@gmail.com>
2016-06-20 17:05:37 -0700

    adds show and index routes and views for posts model

--- time-travel/config/routes.rb
+++ time-travel/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'users#index', as: 'home'
 
+  get '/posts', to: 'posts#index', as: 'posts'
+  get '/posts/:id', to: 'posts#show', as: 'show_post'
+
 end
 
