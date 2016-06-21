a2d54f3194d34fc4abd2a37a490f243dc2e168da
Michael Norelli <mnorelli360@gmail.com>
2016-06-20 17:28:03 -0700

    user added to posts

--- time-travel/config/routes.rb
+++ time-travel/config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'users#index', as: 'home'
+  get '/', to: 'users#home', as: 'home'
+
+
+  ####route for new####
+  get '/signup', to: "users#new", as: "new_user"
+
+  post '/users', to: "users#create"
+
+  get '/users/:id', to: "users#show", as: "user_id"
+
+  ####route for user sign_in#####
+
+  get '/signin', to: "sessions#new"
+
+  post '/sessions', to: 'sessions#create'
+
+
 
   get '/posts', to: 'posts#index', as: 'posts'
   get '/posts/:id', to: 'posts#show', as: 'show_post'
