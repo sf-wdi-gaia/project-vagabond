dab76d80fbcfb8a61cd1050f5be34f64fbc669e0
jenmcphail <mcphail.jen@gmail.com>
2016-06-21 15:00:28 -0700

    updates models; adds seed data for periods

--- time-travel/app/models/period.rb
+++ time-travel/app/models/period.rb
@@ -1,2 +1,4 @@
 class Period < ActiveRecord::Base
+	has_many :posts
+	belongs_to :user
 end
--- time-travel/app/models/post.rb
+++ time-travel/app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  belongs_to :period
+  has_many :periods, through :periodposts
 end
